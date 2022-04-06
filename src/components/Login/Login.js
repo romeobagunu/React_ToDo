07c07ad241b113fa666e0499e564bd10f5e4827d
Romeo Bagunu <romeo.bagunu@outlook.com>
2022-04-06 10:34:25 -0500

    Refactored the code in the login page to redirect the user to the ToDos page once the user is logged in.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,28 +1,24 @@
 import React from 'react'
 import { useAuth } from '../../contexts/AuthContext'
-import { useNavigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 
 export default function Login() {
 
-  const { login } = useAuth();
-  const navigate = useNavigate();
+  const { login, currentUser } = useAuth();
 
-  async function handleAuth() {
-    await login();
-    return navigate("/");
-  }
-
-  return (
+  return !currentUser ? 
+    <>
     <section className="login">
-        <div className="px-4 py-5 my-5 text-center">
+        <div className="px-4 py-5 text-center text-white">
           <h1 className="display-5 fw-bold">Welcome to ToDo!</h1>
           <div className="col-lg-6 mx-auto">
             <p className="lead mb-4">Please click the button below to log in with Github.</p>
             <div className="d-grid d-sm-flex justify-content-sm-center">
-              <button onClick={() => handleAuth()} className="btn btn-success">LOG IN</button>
+              <button onClick={() => login()} className="btn btn-primary">LOG IN</button>
             </div>
           </div>
         </div>
-    </section>
-  )
+    </section> 
+    </> :
+    <Navigate to="/" />
 }
